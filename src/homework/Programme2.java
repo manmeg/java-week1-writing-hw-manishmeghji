be26d867fcb9d39ccd89e734490a5cc632049149
manmeg <manmeg151@gmail.com>
2023-05-17 16:30:00 +0100

    Homework Submitted

---
+++ src/homework/Programme2.java
@@ -0,0 +1,17 @@
+package homework;
+
+/**
+ * 2. Write a Java program to print a face.
+ */
+public class Programme2
+{
+
+        public static void main(String[] args) {
+            System.out.println(" +\"\"\"\"\"+");
+            System.out.println("[| o o |]");
+            System.out.println(" |  ^  | ");
+            System.out.println(" | '_' | ");
+            System.out.println(" +_____+");
+        }
+
+}
---
+++ src/homework/Programme5.java
@@ -0,0 +1,26 @@
+package homework;
+
+/**
+ * 5. Write a Java Program to print below triangle
+ *           *
+ *          * *
+ *         * * *
+ *        * * * *
+ *       * * * * *
+ *      * * * * * *
+ */
+
+public class Programme5
+{
+    public static void main(String[] args) {
+        System.out.println("       *       ");
+        System.out.println("      * *      ");
+        System.out.println("     * * *     ");
+        System.out.println("    * * * *    ");
+        System.out.println("   * * * * *   ");
+        System.out.println("  * * * * * *  ");
+
+
+    }
+
+}
